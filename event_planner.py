68c288b6f1ea32f77cae34a95fce426c35345369
Pieter Edwards <pieterkurtedwards@gmail.com>
2026-02-10 16:44:55 +0000

    Began setting up the function that imports data from the input files

--- event_planner.py
+++ event_planner.py
@@ -4,3 +4,13 @@ class Activity:
         self.time = time
         self.cost = cost
         self.enjoyment = enjoyment
+
+activities_list = []
+
+def process_input_data(filename):
+    f = open(filename)
+    lines = f.readlines()
+
+    f.close()
+
+# def brute_force_search:
