c663ff376608b800d6b5202cbe028c35a2717384
Pieter Edwards <pieterkurtedwards@gmail.com>
2026-02-10 16:37:23 +0000

    Created class for activities

---
+++ event_planner.py
@@ -0,0 +1,6 @@
+class Activity:
+    def __init__(self, name, time, cost, enjoyment):
+        self.name = name
+        self.time = time
+        self.cost = cost
+        self.enjoyment = enjoyment
