00ace216ae6fb8842f143c4d5021f2a19274fcc8
Pieter Edwards <pieterkurtedwards@gmail.com>
2026-02-15 17:10:25 +0000

    Imported intertools library

--- event_planner.py
+++ event_planner.py
@@ -1,3 +1,5 @@
+import intertools
+
 class Activity:
     def __init__(self, name, time, cost, enjoyment):
         self.name = name
@@ -17,8 +19,16 @@ def process_input_data(filename):
     num_of_activities = int(lines[0].strip())
     for i in range(2, (num_of_activities + 2)):
         attributes = lines[i].strip().split()
+
+        # Creates instance of Activity class and popuilates it with data from input file
         new_activity = Activity(attributes[0], attributes[1], attributes[2], attributes[3])
         activities_list.append(new_activity)
                    
+def brute_force_search(activities, time_limit):
+    best_combination = []
+    highest_enjoyment = 0
 
-# def brute_force_search:
+    for size in range(1, len(activities) + 1):
+
+
+# def results
