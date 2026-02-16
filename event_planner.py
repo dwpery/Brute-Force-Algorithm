dc7cae13dec975b8f6dc5beb386b64d85609396a
Pieter Edwards <pieterkurtedwards@gmail.com>
2026-02-16 16:49:21 +0000

    Finished code

--- event_planner.py
+++ event_planner.py
@@ -1,14 +1,16 @@
 import itertools
+import time
 
 class Activity:
     def __init__(self, name, time, cost, enjoyment):
         self.name = name
-        self.time = time
-        self.cost = cost
-        self.enjoyment = enjoyment
+        self.time = int(time)
+        self.cost = int(cost)
+        self.enjoyment = int(enjoyment)
 
 activities_list = []
 time_limit = 0
+budget_limit = 0
 
 def process_input_data(filename):
     # Extracts data from input files
@@ -19,6 +21,7 @@ def process_input_data(filename):
     # Extracts time limit from file
     constraints = lines[1].strip().split()
     time_limit = int(constraints[0])
+    budget_limit = int(constraints[1])
 
     # Converts plain txt into useable objects 
     num_of_activities = int(lines[0].strip())
@@ -28,6 +31,8 @@ def process_input_data(filename):
         # Creates instance of Activity class and popuilates it with data from input file
         new_activity = Activity(attributes[0], attributes[1], attributes[2], attributes[3])
         activities_list.append(new_activity)
+
+    return activities_list, time_limit, budget_limit
                    
 def brute_force_search(activities, time_limit):
     best_combination = []
@@ -53,4 +58,37 @@ def brute_force_search(activities, time_limit):
     return best_combination, best_enjoyment
 
 if __name__ == "__main__":
-    filename = "input_small.txt"
+    # 
+    filename = "input_large.txt"
+    activities_list, time_limit, budget_limit = process_input_data(filename)
+
+    # Performs brute force and calculates execution time
+    start_time = time.time()
+    best_combination, best_enjoyment = brute_force_search(activities_list, time_limit)
+    end_time = time.time()
+    execution_time = end_time - start_time
+
+    print("========================================")
+    print("EVENT PLANNER - RESULTS")
+    print("========================================")
+
+    print(f"\nInput File: {filename}")
+    print(f"Available Time: {time_limit}")
+    print(f"Available Budget: {budget_limit}")
+
+    print(f"\n--- Brute Force Algorithm ---\nSelected Activities:")
+
+    total_time = 0
+    total_cost = 0
+    total_enjoyment = 0
+    for activity in best_combination:
+        print(f"- {activity.name} ({activity.time} hours, £{activity.cost}, enjoyment {activity.enjoyment})")
+        total_time += activity.time
+        total_cost += activity.cost
+        total_enjoyment += activity.enjoyment
+
+    print(f"Total Enjoyment: {total_enjoyment}")
+    print(f"Total Time Used: {total_time}")
+    print(f"Total Cost: {total_cost}")
+
+    print(f"\nExecution Time: {execution_time} seconds")
